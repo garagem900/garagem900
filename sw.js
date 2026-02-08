77062ff938d56ccf1413915624ad22b02cd45c00
garagem900 <creativedesignbebedouro@gmail.com>
2026-02-07 22:47:42 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,37 @@
+const CACHE_NAME = "garagem900-v3";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./logo.png",
+  "./textura.png"
+];
+
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("barrock-cache").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "style.css",
-        "script.js",
-        "logo.png",
-        "textura.png"
-      ]);
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
-
-
 
