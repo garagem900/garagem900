3ed7450dbde3dba648f603724ad30f51d4e8bd37
garagem900 <creativedesignbebedouro@gmail.com>
2026-02-07 22:56:33 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,5 @@
-self.addEventListener("install", e => {
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", e => {
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request));
-});
+self.addEventListener("install", e => self.skipWaiting());
+self.addEventListener("activate", e => self.clients.claim());
+self.addEventListener("fetch", e => e.respondWith(fetch(e.request)));
 
 
